cfb1bf0e27d74a2000c700ae9b2b7d6f7d08d017
Driver <peplinskikrzysztof@gmail.com>
2020-03-22 15:31:38 +0100

    user entity

---
+++ src/main/java/pl/grafikpka/model/User.java
@@ -0,0 +1,55 @@
+package pl.grafikpka.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@Document
+public class User {
+
+    @Id
+    private String id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    private int active;
+
+    private String roles = "";
+
+    private String permissions = "";
+
+    public User(String id, String username, String password, int active, String roles, String permissions) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.active = 1;
+        this.roles = roles;
+        this.permissions = permissions;
+    }
+
+    public List<String> getRoleList(){
+        if (this.roles.length()>0){
+            return Arrays.asList(this.roles.split("'"));
+        }
+        return new ArrayList<>();
+    }
+
+    public List<String> getPermissionList(){
+        if (this.permissions.length()>0){
+            return Arrays.asList(this.permissions.split("'"));
+        }
+        return new ArrayList<>();
+    }
+}
