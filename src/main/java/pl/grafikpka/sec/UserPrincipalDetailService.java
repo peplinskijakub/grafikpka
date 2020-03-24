c4dac1b0a442850222ddd1198dd9791a02055c86
Driver <peplinskikrzysztof@gmail.com>
2020-03-24 17:41:46 +0100

    user detail service

---
+++ src/main/java/pl/grafikpka/sec/UserPrincipalDetailService.java
@@ -0,0 +1,25 @@
+package pl.grafikpka.sec;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import pl.grafikpka.model.User;
+import pl.grafikpka.repository.UserRepository;
+
+public class UserPrincipalDetailService implements UserDetailsService {
+    private UserRepository userRepository;
+
+    public UserPrincipalDetailService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        //find User
+        User user = this.userRepository.findByUsername(s);
+        //trzeba przekonwertować usera na user principal
+        UserPrincipal userPrincipal = new UserPrincipal(user);
+
+        return userPrincipal;
+    }
+}
