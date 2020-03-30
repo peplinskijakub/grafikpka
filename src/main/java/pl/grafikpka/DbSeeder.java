fa0a574307eada3aae7eda701d74889a741c41b3
Driver <peplinskikrzysztof@gmail.com>
2020-03-30 17:05:40 +0200

    db init

---
+++ src/main/java/pl/grafikpka/DbSeeder.java
@@ -0,0 +1,31 @@
+package pl.grafikpka;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import pl.grafikpka.model.User;
+import pl.grafikpka.repository.UserRepository;
+
+import java.util.Arrays;
+import java.util.List;
+@Service
+public class DbSeeder implements CommandLineRunner {
+    private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
+
+    public DbSeeder(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        this.userRepository.deleteAll();
+User pepe = new User("user",passwordEncoder.encode("user123"),"USER","" );
+User admin = new User("admin",passwordEncoder.encode("admin123"),"ADMIN","ACCESS_TEST1,ACCESS_TEST2");
+User manager = new User("manager",passwordEncoder.encode("manager123"),"MANAGER","ACCESS_TEST1");
+
+List<User> users = Arrays.asList(pepe,admin,manager);
+this.userRepository.saveAll(users);
+    }
+}
